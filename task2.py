1e65027eb587e013976ec3458ff170847ca2071c
oliov <oleksii.vorobiov@keemail.me>
2025-12-09 21:27:35 +0000

    fix: changes bfs/dfs to self-written

--- task2.py
+++ task2.py
@@ -1,8 +1,37 @@
 import task1
 
 
-dfs_tree = task1.nx.dfs_tree(task1.leaf_spine, source='leafA1')
-print("Обхід графа в глибину: ", list(dfs_tree.edges()))
+def dfs_recursive(graph, vertex, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(vertex)
+    print(vertex, end=' ')
+    for neighbor in graph[vertex]:
+        if neighbor not in visited:
+            dfs_recursive(graph, neighbor, visited)
 
-bfs_tree = task1.nx.bfs_tree(task1.leaf_spine, source='leafA1')
-print("Обхід графа в ширину: ", list(bfs_tree.edges()))
+
+def bfs_iterative(graph, start):
+    visited = set()
+    queue = [start]
+    #breakpoint()
+    while queue:  
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            print(vertex, end=' ')
+            visited.add(vertex)
+            queue.extend(set(graph[vertex]) - visited)
+
+
+print("DFS:")
+dfs_recursive(task1.nx.to_dict_of_lists(task1.leaf_spine), 'leafA1')
+print("")
+""" dfs_tree = task1.nx.dfs_tree(task1.leaf_spine, source='leafA1')
+print("Обхід графа в глибину: ", list(dfs_tree.edges())) """
+
+
+print("BFS:")
+bfs_iterative(task1.nx.to_dict_of_lists(task1.leaf_spine), 'leafA1')
+print("")
+""" bfs_tree = task1.nx.bfs_tree(task1.leaf_spine, source='leafA1')
+print("Обхід графа в ширину: ", list(bfs_tree.edges())) """
